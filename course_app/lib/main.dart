24f7b27264428f6da53932a425d0aab3b4316a80
deveiap <85376092+deveiap@users.noreply.github.com>
2024-02-14 21:56:11 -0500

    MediaQuery mobile

--- course_app/lib/main.dart
+++ course_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
 import 'model/sidebar.dart';
 
 void main() {
@@ -12,12 +13,104 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: Scaffold(
-        body: Center(
-          child: SidebarRow(item: SidebarItem.sidebarItems[1]),
+        body: SidebarScreen(),
+      ),
+    );
+  }
+}
+
+class SidebarScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: kSidebarBackgroundColor,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(34.0),
+        ),
+      ),
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width * 0.85,
+      padding: EdgeInsets.symmetric(
+        vertical: 35.0,
+        horizontal: 20.0,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('asset/images/profile.jpg'),
+                  radius: 21.0,
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sai Kambampati",
+                      style: kHeadlineLabelStyle,
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "License ends on 21 Jan, 2021",
+                      style: kSearchPlaceholderStyle,
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.08,
+            ),
+            SidebarRow(
+              item: SidebarItem.sidebarItems[0],
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            SidebarRow(
+              item: SidebarItem.sidebarItems[1],
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            SidebarRow(
+              item: SidebarItem.sidebarItems[2],
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            SidebarRow(
+              item: SidebarItem.sidebarItems[3],
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            Spacer(),
+            Row(
+              children: [
+                Image.asset(
+                  'asset/icons/icon-logout.png',
+                  width: 17.0,
+                ),
+                SizedBox(
+                  width: 12.0,
+                ),
+                Text(
+                  "Log out",
+                  style: kSecondaryCalloutLabelStyle,
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
@@ -46,11 +139,7 @@ class SidebarRow extends StatelessWidget {
         Container(
           child: Text(
             item.title.toString(),
-            style: const TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w800,
-              color: Color(0xFF242629),
-            ),
+            style: kCalloutLabelStyle,
           ),
         ),
       ],
